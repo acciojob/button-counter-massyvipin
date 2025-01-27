56453f77f5920cb03a305b9d8b5f9335e20c8335
massyvipin <166917148+massyvipin@users.noreply.github.com>
2025-01-27 16:44:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-
+import React, { useState } from 'react';
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const ClickCounter = () => {
+  // Initialize counter state to 0 using useState hook
+  const [count, setCount] = useState(0);
+
+  // onClick event handler to increment the counter
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+       <button onClick={handleClick}>Click Me</button>
+      <p>Button clicked {count} times.</p>
     </div>
   )
 }
