c2c90c5fb5bc4407b487f95f9cf8c870377962f0
massyvipin <166917148+massyvipin@users.noreply.github.com>
2025-01-27 16:55:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-import React from "react";
 import './../styles/App.css';
 
 const App = () => {
   const ClickCounter = () => {
-  // Initialize counter state to 0 using useState hook
   const [count, setCount] = useState(0);
-
-  // onClick event handler to increment the counter
+    
   const handleClick = () => {
     setCount(count + 1);
   };
